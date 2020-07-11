6ca821a955ea85fa99c3e4439363a0451d1260a2
HM Edgar <hospmex.sistemas@gmail.com>
2020-07-11 11:59:08 -0700

    paralisis content

---
+++ pages/components/Paralisis/Content.vue
@@ -0,0 +1,221 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row row-grid align-items-center">
+        <div class="col-lg-12">
+          <card shadow class="shadow-lg--hover mt-5">
+            <div class="d-flex px-3">
+              <div class="pl-4">
+                <p class="display-2 text-center">Parálisis Facial</p>
+                <p class="display-3 text-center">¿Qué es Parálisis Facial?</p>
+                <p class="text-justify">
+                  La parálisis facial o de Bell, es una enfermedad que ataca el
+                  nervio que controla los movimientos de la cara. Si el nervio
+                  esta “Paralizado” no hay movimientos de los músculos. Casi
+                  todos conocemos o hemos visto personas que se les paraliza uno
+                  o los dos lados de la cara, la cantidad de pacientes es
+                  inmensa.
+                </p>
+                <p class="display-3 text-center">
+                  Incidencia, causas y factores de riesgo
+                </p>
+                <p class="text-justify">
+                  Miles de personas en el mundo son afectados por este problema,
+                  los síntomas son causados por la parálisis del séptimo nervio
+                  craneal que controla los movimientos de la cara. Hay una
+                  infección que es producida por una espiroqueta y causa
+                  muchísimos síntomas en el paciente infectado como son:
+                  Parálisis facial, Migraña, Infartos cerebrales, Artritis,
+                  Vértigo, Hernias en la Columna Vertebral, Síndrome del Túnel
+                  del Carpo, etc.
+                </p>
+                <p class="display-3 text-center">
+                  ¿Se puede atacar este problema?
+                </p>
+                <p class="text-justify">
+                  Claro que sí, nosotros podemos revertir este problema atacando
+                  la causa de la parálisis del nervio que es la infección por la
+                  Espiroqueta, hay tres familias, Leptospira, Borrelia (enf. De
+                  Lyme) y Sífilis.
+                </p>
+                <p class="text-justify">
+                  El tratamiento es solamente con Antibióticos, la duración es
+                  de 4 a 6 meses, hay que vigilar al paciente con exámenes de
+                  Laboratorio cada mes para vigilar su estado de salud por la
+                  ingestión del medicamento.
+                </p>
+                <p class="display-3 text-center">
+                  Factores de riesgo
+                </p>
+                <p class="text-justify">
+                  Cualquiera está predispuesto a ser infectado con esta
+                  infección, cualquier persona ha sido atacada por un mosquito,
+                  pulga, garrapata, piojo, o ha sido transfundido de sangre por
+                  alguna enfermedad o ha sido contaminado por su madre al nacer.
+                </p>
+                <p class="text-justify">
+                  También consideramos que se puede pasar a través del sexo,
+                  frecuentemente observamos que llega un paciente a consulta e
+                  interrogamos al compañero y casi siempre le encontramos
+                  sintomatología.
+                </p>
+                <p class="text-justify">
+                  Uno de los principales transmisores de esta enfermedad
+                  consideramos que es el Hombre, en estudios que se han hecho
+                  por la Universidad de Baja California a través del Doctor
+                  Pineda, en San Quintin, Ensenada y Tijuana, un 51 % de
+                  personas están positivas a LEPTOSPIROSIS.
+                </p>
+                <p class="display-3 text-center">
+                  Síntomas
+                </p>
+                <p class="text-justify">
+                  Frecuentemente el paciente refiere que “se levantó y al abrir
+                  la puerta se le vino un viento que le causo la parálisis”,
+                  esto no es cierto, dio la casualidad que en ese momento se dio
+                  cuenta que traía la parálisis y se lo achaco a esta situación.
+                </p>
+                <p class="text-justify">
+                  La parálisis es frecuentemente en un solo lado pero puede
+                  ocurrir en ambos lados; en muchas ocasiones la parálisis no
+                  solamente ataca la cara, no se puede cerrar el ojo, las
+                  cuerdas vocales, el brazo y el pulmón y el abdomen de ese
+                  mismo lado.
+                </p>
+                <p class="text-justify">
+                  Frecuentemente el paciente no puede hablar bien, no puede
+                  cerrar el ojo de ese lado, si le ataca las cuerdas vocales la
+                  voz le sale diferente y con dificultad, la comida se le va al
+                  lado paralizado o se le sale de la boca, casi siempre es
+                  acompañada por Cefaleas, artralgias (dolor en todos los
+                  huesos), lumbago (dolor de la espalda), vértigo, insomnio,
+                  fatiga crónica y muchos otros síntomas, tiene problemas para
+                  sonreír, hacer muecas o expresiones con la cara. El ojo le
+                  llora.
+                </p>
+                <p class="display-3 text-center">Diagnóstico</p>
+                <p class="text-justify">
+                  La forma segura de hacer el diagnostico de la ESPIROQUETA es a
+                  través del CAMPO OSCURO, para observar la espiroqueta y luego
+                  hacer exámenes para saber si es una Leptospira, una Borrelia o
+                  un Treponema de la Sífilis.
+                </p>
+                <p class="text-justify">
+                  El médico tiene que hacer una Historia Clínica completa, la
+                  parálisis facial se diagnostica viendo los movimientos de los
+                  músculos de la cara, pero si su médico sospecha que puede ser
+                  un Tumor en el cráneo, se le tienen que hacer exámenes de
+                  Laboratorio, Rayos X, Resonancia Magnética, Tomografía, Rayos
+                  X de Cráneo y todo lo que su médico decida que necesita. En
+                  algunas ocasiones se le tiene que hacer una electromiografía.
+                </p>
+                <p class="display-3 text-center">
+                  Pronóstico
+                </p>
+                <p class="text-justify">
+                  En la mayoría de los casos el paciente se “cura de la
+                  parálisis facial en unas pocas semanas”, esto es mentira, se
+                  mejora de su parálisis pero no se cura, continúa con la
+                  sensación leve en la cara y los síntomas colaterales como son:
+                  fatiga crónica, artralgias generalizadas, Cefaleas, insomnio,
+                  vértigo, etc. continúan hasta que se le de su tratamiento
+                  definitivo para curar la causa de su problema que es una
+                  Bacteria que casi siempre es una Espiroqueta (Leptospira,
+                  Treponema o Borrelia Burgdorferi).
+                </p>
+                <p class="display-3 text-center">
+                  Complicaciones
+                </p>
+                <p class="text-justify">
+                  Si el paciente recibe el tratamiento, no hay complicaciones,
+                  pero si no lo recibe continuara con su parálisis leve, casi
+                  indetectable pero otros síntomas continuaran como son
+                  Artritis, Migraña, fatiga crónica, vértigo, Lumbago, insomnio,
+                  irritabilidad, etc.
+                </p>
+                <p class="display-3 text-center">
+                  Tratamiento en el Hospital México
+                </p>
+                <p class="text-justify">
+                  En nuestro Hospital tenemos 20 años de aceptar a los Pacientes
+                  que vienen con Parálisis Facial, pero al administrarles el
+                  tratamiento también les curamos de Leptospirosis, la
+                  Enfermedad de Lyme o Borreliosis o por el Treponema.
+                </p>
+                <p class="text-justify">
+                  Se les administra antibióticos por cuatro a seis meses y el
+                  paciente queda curado PARA TODA LA VIDA si no se reinfecta.
+                  Nuestros tratamientos son a base de ANTIBIÓTICOS, nuestra
+                  experiencia nos ha enseñado que si el paciente se toma bien su
+                  medicamento, casi siempre se cura. Frecuentemente
+                  administramos Claritromicina, Tetraciclina, Penicilina, etc.
+                  Todo depende de la condición del paciente y la aceptación de
+                  los Medicamentos.
+                </p>
+                <p class="text-justify">
+                  El paciente siempre necesita su tratamiento, su Parálisis
+                  Facial puede mejorar pero nunca se cura si no recibe
+                  tratamiento, el paciente siempre sentirá cosas raras en su
+                  cara y nunca regresara a ser la de antes, casi siempre se le
+                  notara algo, o sus amigos o su médico le notaran que jala su
+                  cara a un lado, o que no está derecha. Si su párpado no se le
+                  cierra completamente, usted necesitara Lágrimas Artificiales o
+                  en algunas ocasiones un parche que le tape el ojo hasta que se
+                  recupere un poco y pueda cerrar más sus párpados.
+                </p>
+                <p class="display-3 text-center">Prevención</p>
+                <p class="text-justify">
+                  Existen artículos que recomiendan muchas cosas para prevenir,
+                  pero nosotros consideramos que son ilógicos o difícil de
+                  cumplir, usted se puede contaminar en su casa con sus
+                  familiares, de su perro, en su jardín, en la Escuela, en los
+                  asientos de lugares públicos a través de piojos del cuerpo
+                  (escabiasis), piojos del cabello, todo lo que se alimente de
+                  nuestra sangre puede haber estado con un paciente o un animal
+                  contaminado y todo lo que lo pique a usted puede ir a picar a
+                  un vecino, un familiar o a quien viva en su casa o su
+                  compañero de trabajo o de Escuela.
+                </p>
+                <p class="text-justify">
+                  La única forma de Prevención es curarse definitivamente,
+                  vigilar o examinar a los perros, gatos y otros familiares
+                  viviendo con usted, tratarse los síntomas que la acompañan.
+                  Normalmente antes de iniciar la parálisis ya el paciente tenía
+                  muchos años de presentar otros síntomas de la Espiroqueta,
+                  como son: Migraña, Artritis, Lumbago, adormecimiento de
+                  brazos, insomnio o dormir mucho, irritabilidad (se pelea con
+                  muchas personas), vértigo, etc.
+                </p>
+                <p class="display-3 text-center">
+                  Conocimiento Médico
+                </p>
+                <p class="text-justify">
+                  Existen muy pocos Médicos que conocen la infección por
+                  Espiroquetas (Borrelia, Leptospira, Treponema.) y cuando
+                  escuchan algo de esta infección piensan que es algo muy raro
+                  puesto que no lo relaciona con enfermedades que son muy
+                  comunes en nuestro medio tales como artritis, migraña,
+                  parálisis facial o fatiga crónica. En nuestro Centro tenemos
+                  la capacidad de mostrarle al paciente la Bacteria mediante
+                  microcopia de campo oscuro para que así comience a comprender
+                  más su enfermedad. Es muy agradable poder sanar a una persona
+                  que le han dicho que su enfermedad es incurable, que ha
+                  sufrido por años y años consultando a Médicos Especialistas
+                  que le dicen que “NO HAY CURACIÓN PARA SU PROBLEMA” o que es
+                  el estrés o producto de su imaginación y lo envían con un
+                  Siquiatra pues ellos no saben lo que tiene el paciente y
+                  probablemente es su mente
+                </p>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'ContentM',
+}
+</script>
---
+++ pages/components/Paralisis/Description.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row row-grid align-items-center">
+        <div class="col-lg-12">
+          <card shadow class="shadow-lg--hover mt-5">
+            <div class="d-flex px-3">
+              <div class="pl-4">
+                <p class="h5 text-justify">
+                  Las Espiroquetas y su relación con parálisis facial. Con este
+                  nuevo tratamiento se cura el 90% de los pacientes con
+                  parálisis facial tratados.
+                </p>
+                <h5 class="title text-success">LLAME AHORA</h5>
+                <ul class="list-unstyled mt-2">
+                  <li class="py-2">
+                    <div class="d-flex align-items-center">
+                      <a
+                        class="nav-link nav-link-icon"
+                        href="tel:16195814081"
+                        target="_blank"
+                        rel="noopener"
+                        data-toggle="tooltip"
+                        title="LLAMENOS"
+                      >
+                        <h6 class="">
+                          A California desde fuera de USA 1(619) 581-4081
+                        </h6>
+                      </a>
+                    </div>
+                  </li>
+                  <li class="py-2">
+                    <div class="d-flex align-items-center">
+                      <a
+                        class="nav-link nav-link-icon"
+                        href="tel:16195814081"
+                        target="_blank"
+                        rel="noopener"
+                        data-toggle="tooltip"
+                        title="Call Us"
+                      >
+                        <h6 class="mb-0">A California en USA (619) 581-4081</h6>
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'DescriptionM',
+}
+</script>
