7025638f18818b82401a117f549637cc628f3e34
HM Edgar <hospmex.sistemas@gmail.com>
2020-07-11 13:57:35 -0700

    paralisis content

---
+++ pages/components/Sindrome/Content.vue
@@ -0,0 +1,127 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row row-grid align-items-center">
+        <div class="col-lg-12">
+          <card shadow class="shadow-lg--hover mt-5">
+            <div class="d-flex px-3">
+              <div class="pl-4">
+                <p class="display-2 text-center">
+                  Síndrome del Túnel del Carpo (STC)
+                </p>
+                <p class="display-3 text-center">
+                  ¿Qué es Síndrome del Túnel del Carpo (STC)?
+                </p>
+                <p class="text-justify">
+                  El síndrome del túnel del carpo es una inflamación del nervio
+                  mediano a su paso por el túnel carpiano. El túnel del CARPO es
+                  un canal que pasa por la cara anterior de los huesos de la
+                  muñeca y el ligamento anular del carpo, por el cual pasa el
+                  nervio mediano (uno de los nervios de la mano). Cuando el
+                  nervio mediano se inflama, es comprimido por los tendones y
+                  viene el dolor al movimiento.
+                </p>
+                <p class="display-3 text-center">
+                  ¿Cuáles son las causas del síndrome del túnel del carpo?
+                </p>
+                <p class="text-justify">
+                  Son tantos los tendones que atraviesan dicho canal que el
+                  nervio mediano dispone de un espacio muy justo en su zona
+                  central. Si por cualquier causa se inflama el nervio,
+                  disminuye aún más este espacio, entonces resulta comprimido el
+                  nervio. Las causa de esta inflamación puede ser una infección
+                  por Espiroqueta, puesto que cuando se da tratamiento con
+                  antibióticos el dolor cede inmediatamente.
+                </p>
+                <p class="text-justify">
+                  Se asocia a enfermedades reumáticas, y el paciente lo
+                  relaciona con actividades repetitivas de la mano pero no se da
+                  cuenta que casi siempre viene asociado con Artritis, Migraña,
+                  Lumbago, etc.
+                </p>
+                <p class="display-3 text-center">
+                  ¿A quién afecta el síndrome del túnel del carpo?
+                </p>
+                <p class="text-justify">
+                  El síndrome del túnel del carpo (STC) es una patología muy
+                  frecuente, que afecta a una gran cantidad de la población en
+                  general y cuando afecta a los trabajadores, estos se lo
+                  achacan al trabajo en especifico, pero este síndrome viene con
+                  una afectación generalizada de todo el cuerpo, casi siempre
+                  tienen artralgias o sensación dolorosa en otra parte y los
+                  Cirujanos rápido los operan y la mayoría de pacientes que
+                  hemos visto en nuestra consulta están arrepentidos de la
+                  Cirugía, puesto que continúan con molestias en la zona, además
+                  también presentan artralgias, Lumbago, Migraña, etc. que no se
+                  las quitan después de la cirugía.
+                </p>
+                <p class="display-3 text-center">
+                  Síntomas del síndrome del túnel del carpo
+                </p>
+                <p class="text-justify">
+                  Al inicio la mayoría de los pacientes con síndrome del túnel
+                  del carpo se quejan de dolor en la región de la muñeca y
+                  antebrazo, asociando sensación de acorchamiento, calambres y
+                  hormigueos en los dedos pulgar, índice, medio y parte del
+                  anular. Estos síntomas típicamente son al movimiento. Si no se
+                  trata con antibióticos, aparecerá debilidad y atrofia de
+                  algunos músculos de la mano.
+                </p>
+                <p class="display-3 text-center">
+                  ¿Cómo se diagnostica el síndrome del túnel del carpo?
+                </p>
+                <p class="text-justify">
+                  El Paciente llega con el Médico y se queja de molestias al
+                  movimiento en esa región, inmediatamente viene el diagnostico
+                  de SÍNDROME DEL TÚNEL DEL CARPO, pero el Médico no se da
+                  cuenta que el paciente también presenta Artralgias, migraña,
+                  lumbago, depresión, insomnio, etc. en esta situación lo
+                  tenemos que relacionar con una infección por ESPIROQUETAS, ya
+                  sea Leptospira, sífilis o Enfermedad de lyme (Borreliosis).
+                </p>
+                <p class="display-3 text-center">
+                  Tratamiento en el Hospital México
+                </p>
+                <p class="text-justify">
+                  En nuestra experiencia siempre se cura con Antibióticos. Nunca
+                  hemos tenido lo necesidad de practicarle una Cirugía. Tampoco
+                  le recetamos una férula puesto que el paciente responde
+                  inmediatamente al Antibiótico.
+                </p>
+                <p class="text-justify">
+                  Los síntomas que le acompañan, también ceden a los 4 a 6 meses
+                  de tratamiento.
+                </p>
+                <p class="display-3 text-center">
+                  Tratamiento quirúrgico
+                </p>
+                <p class="text-justify">
+                  Nunca lo recomendamos aunque tenemos la capacidad de hacerla
+                  fácilmente en nuestro Hospital.
+                </p>
+                <p class="display-3 text-center">
+                  ¿Cuáles son los resultados?
+                </p>
+                <p class="text-justify">
+                  Prácticamente todos los pacientes experimentan una mejoría
+                  satisfactoria durante el primer mes, siendo poco probables las
+                  recidivas si nuestro paciente cumple bien su tratamiento de
+                  cuatro a seis meses con antibióticos, no se le prescriben
+                  analgésicos ni corticoesteroides. Nosotros consideramos que
+                  esta molestia es parte de la multitud de síntomas que
+                  acompañan a la Leptospirosis, la Borreliosis o Enfermedad de
+                  Lyme y a la Sífilis.
+                </p>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'ContentS',
+}
+</script>
