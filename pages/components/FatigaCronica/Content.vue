91533bb1505f470846c9a7fd59a4a2f2f4826cf6
HM Edgar <hospmex.sistemas@gmail.com>
2020-07-16 11:05:02 -0700

    fatiga cronica

---
+++ pages/components/FatigaCronica/Content.vue
@@ -0,0 +1,163 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row row-grid align-items-center">
+        <div class="col-lg-12">
+          <card shadow class="shadow-lg--hover mt-5">
+            <div class="d-flex px-3">
+              <div class="pl-4">
+                <p class="display-2 text-center">
+                  Síndrome de fatiga crónica
+                </p>
+                <p class="display-3 text-center">
+                  ¿Qué es el Síndrome de fatiga crónica?
+                </p>
+                <p class="text-justify">
+                  Se dice que para ser un síndrome tiene el paciente que
+                  sufrirlo por mas de 6 meses, pero nosotros consideramos que la
+                  fatiga crónica solamente es un síntoma de otras enfermedades
+                  crónicas que el Medico no es capaz de diagnosticar como lo
+                  son: Leptospirosis, Borreliosis o Enf. de Lyme, Treponema o
+                  Sífilis y otras.
+                </p>
+                <p class="text-justify">
+                  Algunos de los otros síntomas, enfermedades o infecciones que
+                  pueden causarle: Insuficiencia Adrenal, Cáncer, SIDA,
+                  Hepatopatias, Enf. Renal, Tiroides, Enf. Renal, nódulos
+                  linfaticos, virus, Clamidia, Epstein-Barr, Mononucleosis
+                  infecciosa, etc.
+                </p>
+                <p class="display-3 text-center">
+                  Diagnóstico
+                </p>
+                <p class="text-justify">
+                  El Medico tiene que esforzarse para encontrar la causa del
+                  Sindrome Síntoma de Fatiga Crónica, porque la sensación que
+                  siente el paciente de perdida de la fuerza y que tiene que
+                  descanzar y dormir mucho, solamente es el reflejo de que algo
+                  malo esta en su cuerpo y debemos curar la infección que la
+                  esta causando.
+                </p>
+                <p class="text-justify">
+                  Hemos observado que frecuentemente el síntoma de Fatiga
+                  Crónica es tan solo un síntoma de la situación infecciosa del
+                  paciente, casi siempre es acompañado de Migraña, Artralgias,
+                  Hernias en la Columna Vertebral, Parálisis Facial, Lupus,
+                  Fibromialgia, etc. y el Medico le hace solamente un
+                  Diagnostico e ignora los demás, y casi siempre los familiares
+                  están contaminados con la misma infección y su Medico no
+                  interroga a su familia.
+                </p>
+                <p class="display-3 text-center">
+                  Debe saber que...
+                </p>
+                <p class="text-justify">
+                  La ciencia Medica siempre ha rechazado los antibióticos cuando
+                  se trata de problemas Mentales, neurológicos artralgicos o
+                  sintomatología de lo que no sabe. En nuestra experiencia hemos
+                  visto que muchos problemas neurológicos, artralgicos y
+                  Psiquiátricos se benefician del tratamiento con antibióticos
+                  por ser causados por una infección que no es muy detectable
+                  por los métodos tradicionales de la Ciencia Moderna, o si es
+                  de muy difícil diagnostico estamos acostumbrados a decir que
+                  es Autoinmune y con eso ya tenemos un diagnostico decente para
+                  nuestros pacientes y muy intelectuales le decimos “ya no se
+                  puede hacer nada, váyase para su casa y usted es el culpable
+                  de todas sus enfermedades”.
+                </p>
+                <p class="text-justify">
+                  La teoría microbiana de la enfermedad es una teoría que
+                  propone que los microrganismos son la causa de muchas
+                  enfermedades, pero no se ha intentado a los antibióticos para
+                  curar los problemas neurológicos, artriticos y siquiátricos
+                  que para nosotros son síntomas de enfermedad generalizada por
+                  bacterias en todo nuestro cuerpo las cuales se pueden
+                  visualizar algunas en el microscopio por medio del Campo
+                  Oscuro.
+                </p>
+                <p class="text-justify">
+                  Aunque en tiempos antiguos fue muy controvertida cuando se
+                  propuso, es ahora fundamental en la medicina moderna y la
+                  microbiología clínica, conduciendo a innovaciones tan
+                  importantes como los antibióticos y las prácticas higiénicas.
+                </p>
+                <p class="text-justify">
+                  El médico italiano Agostino Bassi es a menudo reconocido por
+                  haber expuesto la teoría microbiana de la enfermedad por
+                  primera vez, basándose en sus observaciones en la letal y
+                  epidémica enfermedad de la muscardina de los gusanos de seda.
+                  En 1835 culpó específicamente de las muertes de los insectos a
+                  un agente vivo y contagioso que era visible a simple vista
+                  como multitud de esporas polvorientas; estos hongos
+                  microscópicos fueron posteriormente llamados Beauveria
+                  bassiana en su honor.
+                </p>
+                <p class="text-justify">
+                  Louis Pasteur demostró entre 1860 y 1864 que la fermentación y
+                  el desarrollo de microrganismos en caldos de nutrientes no
+                  procedía de la generación espontánea. Expuso caldo recién
+                  hervido al aire en vasijas que contenían un filtro para
+                  impedir que todas las partículas pasaran al medio de cultivo:
+                  e incluso sin ningún filtro, con el aire siendo admitido por
+                  un largo y tortuoso tubo que no dejaría pasar partículas de
+                  polvo. No creció nada en los caldos, por tanto los organismos
+                  vivos que crecieron en estos caldos venían de fuera, como
+                  esporas en el polvo, en vez de ser generadas dentro del caldo.
+                </p>
+                <p class="text-justify">
+                  Louis Pasteur demostró entre 1860 y 1864 que la fermentación y
+                  el desarrollo de microrganismos en caldos de nutrientes no
+                  procedía de la generación espontánea. Expuso caldo recién
+                  hervido al aire en vasijas que contenían un filtro para
+                  impedir que todas las partículas pasaran al medio de cultivo:
+                  e incluso sin ningún filtro, con el aire siendo admitido por
+                  un largo y tortuoso tubo que no dejaría pasar partículas de
+                  polvo. No creció nada en los caldos, por tanto los organismos
+                  vivos que crecieron en estos caldos venían de fuera, como
+                  esporas en el polvo, en vez de ser generadas dentro del caldo.
+                </p>
+                <p class="display-3 text-center">
+                  Tratamiento en el Hospital México
+                </p>
+                <p class="text-justify">
+                  En este tiempo las famosas “Enfermedades Autoinmunes han
+                  descartado los antibióticos de las enfermedades que no
+                  conocemos que las causa” o se les da muy poco tiempo el
+                  antibiótico y después de una semana decimos que no sirven para
+                  esas enfermedades, pensamos que en ciertas ocasiones se deben
+                  administrar por meses para lograr acabar con algunas
+                  infecciones que están muy arraigadas en el cuerpo como es en
+                  el caso de la Artritis, parálisis facial, migraña,
+                  fibromialgia, vértigo, Lupus, Hernias Lumbares, etc. También
+                  hemos sido atacados por algunos Médicos que consideran
+                  imposible de creer los procedimientos que estamos efectuando
+                  tratando estas Enfermedades con Antibióticos.
+                </p>
+                <p class="text-justify">
+                  Una persona muy cercana me contó que cuando salió la
+                  Penicilina, todo mundo se la administraba para curarse
+                  cualquier infección que tuviera en su cuerpo, pienso que en
+                  este tiempo tenemos que hacer lo mismo con los diferentes
+                  Antibióticos Modernos que existen hoy para curar “lo
+                  incurable”.
+                </p>
+                <p class="text-justify">
+                  Para concluir, decimos que el Síndrome de Fatiga crónica no es
+                  Autoinmune, es solamente la sintomatología de una infección
+                  que el Médico no puede Diagnosticar, y que puede ser debido a
+                  alguna infección que esta diseminada en todo nuestro cuerpo y
+                  que una de las causantes pueden ser las ESPIROQUETAS.
+                </p>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'ContentFC',
+}
+</script>
